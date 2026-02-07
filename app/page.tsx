71017185741f8223e492a9b808bacaadef1b8ec6
Nicole Walzl <nicole.walzl@Nicoles-MacBook-Air.local>
2026-02-07 22:17:28 +0100

    remove duplicate Home export

--- app/page.tsx
+++ app/page.tsx
@@ -61,13 +61,11 @@ export default function Home() {
         </div>
       </main>
     </div>
-  );
-}
 export default function Home() {
   return (
     <main style={{ padding: 40 }}>
-      <h1>Delulu Universe 🌌</h1>
+      <h1>Delulu Universe</h1>
       <p>First working deployment.</p>
     </main>
-  );
+  )
 }
